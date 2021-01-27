e3efdc940b65c09c4af9f6aeb0d54dcc035b1568
Ido Ben-Hur <idoybh2@gmail.com>
2021-01-27 17:10:50 +0530

    RavenLair: Only allow edge lights on AOD when AOD is enabled [2/2]
    Change-Id: I8771366507c6b061780b9891f3c726f0d908f877

--- src/com/raven/lair/fragments/AmbientDecorSettings.java
+++ src/com/raven/lair/fragments/AmbientDecorSettings.java
@@ -33,11 +33,13 @@ public class AmbientDecorSettings extends SettingsPreferenceFragment
     private ColorPickerPreference mEdgeLightColorPreference;
     private SystemSettingSeekBarPreference mEdgeLightDurationPreference;
     private SystemSettingSeekBarPreference mEdgeLightRepeatCountPreference;
+    private SystemSettingSwitchPreference mAmbientPref;
     private ListPreference mColorMode;
 
     private static final String NOTIFICATION_PULSE_COLOR = "ambient_notification_light_color";
     private static final String AMBIENT_LIGHT_DURATION = "ambient_light_duration";
     private static final String AMBIENT_LIGHT_REPEAT_COUNT = "ambient_light_repeat_count";
+        private static final String KEY_AMBIENT = "ambient_notification_light_enabled";
     private static final String PULSE_COLOR_MODE_PREF = "ambient_notification_light_color_mode";
 
     @Override
@@ -48,6 +50,15 @@ public class AmbientDecorSettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.ambient_decor);
 
         PreferenceScreen prefScreen = getPreferenceScreen();
+
+       mAmbientPref = (SystemSettingSwitchPreference) findPreference(KEY_AMBIENT);
+        boolean aodEnabled = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.DOZE_ALWAYS_ON, 0, UserHandle.USER_CURRENT) == 1;
+        if (!aodEnabled) {
+            mAmbientPref.setChecked(false);
+            mAmbientPref.setEnabled(false);
+            mAmbientPref.setSummary(R.string.aod_disabled);
+        }
 
         mEdgeLightRepeatCountPreference = (SystemSettingSeekBarPreference) findPreference(AMBIENT_LIGHT_REPEAT_COUNT);
         mEdgeLightRepeatCountPreference.setOnPreferenceChangeListener(this);
